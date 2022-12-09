267fc8759936cf3c701d70f98bd511b4efd41980
sophie mattacks <s.mattacks@hotmail.co.uk>
2022-12-09 11:32:58 +0000

    retrieve user’s lists

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,6 +5,7 @@ class UserController < ApplicationController
   end
 
   def login
+    #Finds stored login details and if user not found, redirects to login page.
     user = User.find_by(user_name: params[:user_name])
     if user.nil?
       # @error = "User does not exist" # TODO: Fix me
@@ -12,16 +13,25 @@ class UserController < ApplicationController
       return
     end
 
+    #Log user in by storing user_name in the session.
     session[:user_name] = user.user_name
     redirect_to '/dashboard'
-
-    # secret_santa_lists = user.secret_santa_lists
-    # puts secret_santa_lists
   end
 
   def dashboard
+    #Redirects to homepage if user isn't stored in the session.
     redirect_to '/' if session[:user_name].nil?
     @user_name = session[:user_name]
+
+    #Finds User's made lists
+    user = User.find_by(user_name: @user_name)
+    @secret_santa_lists = user.secret_santa_lists
+
+    @to_buy_for = SantaListParticipant.where(sender_id_id: user.id)
+    @to_buy_for.each do |p|
+      puts p.receiver
+    end
+
   end
 
   def logout
