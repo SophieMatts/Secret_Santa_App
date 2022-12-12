accf11abd5d277ac17c45dc265ab0e760348a9e5
sophie mattacks <s.mattacks@hotmail.co.uk>
2022-12-12 11:19:57 +0000

    CSS for wish list and other CSS final touches

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,7 @@
 class UserController < ApplicationController
 
   def index
+    #Redirect to dashboard, not sign-in page, if that user is logged in.
     redirect_to '/dashboard' unless session[:user_name].nil?
   end
 
@@ -23,17 +24,18 @@ class UserController < ApplicationController
     redirect_to '/' if session[:user_name].nil?
     @user_name = session[:user_name]
 
-    #Finds User's made lists
+    #Finds User's made lists.
     @user = User.find_by(user_name: @user_name)
     @secret_santa_lists = @user.secret_santa_lists
 
-    #Finds person to buy for
+    #Finds person to buy for.
     @to_buy_for = SantaListParticipant
                     .where(sender_id_id: @user.id) #I'm the sender
                     .where.not(receiver_id_id: nil) #There is a reciever
   end
 
   def save_wishlist
+    #Find's user's wishlist, saves item, redirects back to root (dashboard)
     wishlist = params[:wishlist]
     user = User.find_by(user_name: session[:user_name])
     user.wish_list = wishlist
